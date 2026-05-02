36c8c1d2ff89f0f25d0b9c93d615905b21873a93
KOBEJAMESMAWANAY <2024-2-00378@lpunetwork.edu.ph>
2026-05-02 16:19:11 +0800

    BASTA OKAY NAAA

--- CyberTrack_Administrator/MenuButtonForm.Designer.cs
+++ CyberTrack_Administrator/MenuButtonForm.Designer.cs
@@ -30,21 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuButton_Administrator));
             this.grpMenuButton_Administrator = new System.Windows.Forms.GroupBox();
-            this.dgvMenuButtonLine2_Administrator = new System.Windows.Forms.Label();
-            this.dgvMenuButtonLine1_Administrator = new System.Windows.Forms.Label();
-            this.dgvMenuButtonLOGOUT_Administrator = new System.Windows.Forms.Button();
-            this.lblMenuButton_Administrator = new System.Windows.Forms.Label();
+            this.dgvMenuButton_Administrator = new System.Windows.Forms.DataGridView();
+            this.txtMenuButtonList_Administrator = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtMenuButtonSwitchUser_Administrator = new System.Windows.Forms.DataGridViewButtonColumn();
             this.grbUserLogo_Administrator = new System.Windows.Forms.GroupBox();
             this.picUserPhoto_Administrator = new System.Windows.Forms.PictureBox();
             this.lblUserRole_Administrator = new System.Windows.Forms.Label();
             this.lblUserName_Administrator = new System.Windows.Forms.Label();
-            this.dgvMenuButton_Administrator = new System.Windows.Forms.DataGridView();
-            this.txtMenuButtonList_Administrator = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.txtMenuButtonSwitchUser_Administrator = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.dgvMenuButtonLine2_Administrator = new System.Windows.Forms.Label();
+            this.dgvMenuButtonLine1_Administrator = new System.Windows.Forms.Label();
+            this.dgvMenuButtonLOGOUT_Administrator = new System.Windows.Forms.Button();
+            this.lblMenuButton_Administrator = new System.Windows.Forms.Label();
             this.grpMenuButton_Administrator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenuButton_Administrator)).BeginInit();
             this.grbUserLogo_Administrator.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picUserPhoto_Administrator)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMenuButton_Administrator)).BeginInit();
             this.SuspendLayout();
             // 
             // grpMenuButton_Administrator
@@ -55,58 +55,41 @@
             this.grpMenuButton_Administrator.Controls.Add(this.dgvMenuButtonLine1_Administrator);
             this.grpMenuButton_Administrator.Controls.Add(this.dgvMenuButtonLOGOUT_Administrator);
             this.grpMenuButton_Administrator.Controls.Add(this.lblMenuButton_Administrator);
-            this.grpMenuButton_Administrator.Location = new System.Drawing.Point(99, 24);
+            this.grpMenuButton_Administrator.Location = new System.Drawing.Point(12, 12);
             this.grpMenuButton_Administrator.Name = "grpMenuButton_Administrator";
-            this.grpMenuButton_Administrator.Size = new System.Drawing.Size(602, 553);
+            this.grpMenuButton_Administrator.Size = new System.Drawing.Size(610, 587);
             this.grpMenuButton_Administrator.TabIndex = 1;
             this.grpMenuButton_Administrator.TabStop = false;
             // 
-            // dgvMenuButtonLine2_Administrator
+            // dgvMenuButton_Administrator
             // 
-            this.dgvMenuButtonLine2_Administrator.AutoSize = true;
-            this.dgvMenuButtonLine2_Administrator.Location = new System.Drawing.Point(95, 473);
-            this.dgvMenuButtonLine2_Administrator.Name = "dgvMenuButtonLine2_Administrator";
-            this.dgvMenuButtonLine2_Administrator.Size = new System.Drawing.Size(415, 13);
-            this.dgvMenuButtonLine2_Administrator.TabIndex = 10;
-            this.dgvMenuButtonLine2_Administrator.Text = "____________________________________________________________________";
+            this.dgvMenuButton_Administrator.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvMenuButton_Administrator.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMenuButton_Administrator.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.txtMenuButtonList_Administrator,
+            this.txtMenuButtonSwitchUser_Administrator});
+            this.dgvMenuButton_Administrator.Location = new System.Drawing.Point(98, 173);
+            this.dgvMenuButton_Administrator.Name = "dgvMenuButton_Administrator";
+            this.dgvMenuButton_Administrator.Size = new System.Drawing.Size(412, 292);
+            this.dgvMenuButton_Administrator.TabIndex = 89;
             // 
-            // dgvMenuButtonLine1_Administrator
+            // txtMenuButtonList_Administrator
             // 
-            this.dgvMenuButtonLine1_Administrator.AutoSize = true;
-            this.dgvMenuButtonLine1_Administrator.Location = new System.Drawing.Point(95, 124);
-            this.dgvMenuButtonLine1_Administrator.Name = "dgvMenuButtonLine1_Administrator";
-            this.dgvMenuButtonLine1_Administrator.Size = new System.Drawing.Size(415, 13);
-            this.dgvMenuButtonLine1_Administrator.TabIndex = 9;
-            this.dgvMenuButtonLine1_Administrator.Text = "____________________________________________________________________";
+            this.txtMenuButtonList_Administrator.HeaderText = "LIST";
+            this.txtMenuButtonList_Administrator.Name = "txtMenuButtonList_Administrator";
+            this.txtMenuButtonList_Administrator.ReadOnly = true;
             // 
-            // dgvMenuButtonLOGOUT_Administrator
+            // txtMenuButtonSwitchUser_Administrator
             // 
-            this.dgvMenuButtonLOGOUT_Administrator.BackColor = System.Drawing.Color.Black;
-            this.dgvMenuButtonLOGOUT_Administrator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dgvMenuButtonLOGOUT_Administrator.ForeColor = System.Drawing.Color.White;
-            this.dgvMenuButtonLOGOUT_Administrator.Location = new System.Drawing.Point(98, 489);
-            this.dgvMenuButtonLOGOUT_Administrator.Name = "dgvMenuButtonLOGOUT_Administrator";
-            this.dgvMenuButtonLOGOUT_Administrator.Size = new System.Drawing.Size(412, 46);
-            this.dgvMenuButtonLOGOUT_Administrator.TabIndex = 8;
-            this.dgvMenuButtonLOGOUT_Administrator.Text = "LOG OUT";
-            this.dgvMenuButtonLOGOUT_Administrator.UseVisualStyleBackColor = false;
-            // 
-            // lblMenuButton_Administrator
-            // 
-            this.lblMenuButton_Administrator.AutoSize = true;
-            this.lblMenuButton_Administrator.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMenuButton_Administrator.Location = new System.Drawing.Point(114, 150);
-            this.lblMenuButton_Administrator.Name = "lblMenuButton_Administrator";
-            this.lblMenuButton_Administrator.Size = new System.Drawing.Size(364, 25);
-            this.lblMenuButton_Administrator.TabIndex = 3;
-            this.lblMenuButton_Administrator.Text = "ADMIN USER SWITCH CONTROL";
+            this.txtMenuButtonSwitchUser_Administrator.HeaderText = "SwitchUser";
+            this.txtMenuButtonSwitchUser_Administrator.Name = "txtMenuButtonSwitchUser_Administrator";
             // 
             // grbUserLogo_Administrator
             // 
             this.grbUserLogo_Administrator.Controls.Add(this.picUserPhoto_Administrator);
             this.grbUserLogo_Administrator.Controls.Add(this.lblUserRole_Administrator);
             this.grbUserLogo_Administrator.Controls.Add(this.lblUserName_Administrator);
-            this.grbUserLogo_Administrator.Location = new System.Drawing.Point(211, 47);
+            this.grbUserLogo_Administrator.Location = new System.Drawing.Point(211, 42);
             this.grbUserLogo_Administrator.Name = "grbUserLogo_Administrator";
             this.grbUserLogo_Administrator.Size = new System.Drawing.Size(183, 74);
             this.grbUserLogo_Administrator.TabIndex = 88;
@@ -143,43 +126,60 @@
             this.lblUserName_Administrator.TabIndex = 5;
             this.lblUserName_Administrator.Text = "Skibidi Toilet";
             // 
-            // dgvMenuButton_Administrator
+            // dgvMenuButtonLine2_Administrator
             // 
-            this.dgvMenuButton_Administrator.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dgvMenuButton_Administrator.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvMenuButton_Administrator.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.txtMenuButtonList_Administrator,
-            this.txtMenuButtonSwitchUser_Administrator});
-            this.dgvMenuButton_Administrator.Location = new System.Drawing.Point(98, 178);
-            this.dgvMenuButton_Administrator.Name = "dgvMenuButton_Administrator";
-            this.dgvMenuButton_Administrator.Size = new System.Drawing.Size(412, 292);
-            this.dgvMenuButton_Administrator.TabIndex = 89;
+            this.dgvMenuButtonLine2_Administrator.AutoSize = true;
+            this.dgvMenuButtonLine2_Administrator.Location = new System.Drawing.Point(95, 468);
+            this.dgvMenuButtonLine2_Administrator.Name = "dgvMenuButtonLine2_Administrator";
+            this.dgvMenuButtonLine2_Administrator.Size = new System.Drawing.Size(415, 13);
+            this.dgvMenuButtonLine2_Administrator.TabIndex = 10;
+            this.dgvMenuButtonLine2_Administrator.Text = "____________________________________________________________________";
             // 
-            // txtMenuButtonList_Administrator
+            // dgvMenuButtonLine1_Administrator
             // 
-            this.txtMenuButtonList_Administrator.HeaderText = "LIST";
-            this.txtMenuButtonList_Administrator.Name = "txtMenuButtonList_Administrator";
-            this.txtMenuButtonList_Administrator.ReadOnly = true;
+            this.dgvMenuButtonLine1_Administrator.AutoSize = true;
+            this.dgvMenuButtonLine1_Administrator.Location = new System.Drawing.Point(95, 119);
+            this.dgvMenuButtonLine1_Administrator.Name = "dgvMenuButtonLine1_Administrator";
+            this.dgvMenuButtonLine1_Administrator.Size = new System.Drawing.Size(415, 13);
+            this.dgvMenuButtonLine1_Administrator.TabIndex = 9;
+            this.dgvMenuButtonLine1_Administrator.Text = "____________________________________________________________________";
             // 
-            // txtMenuButtonSwitchUser_Administrator
+            // dgvMenuButtonLOGOUT_Administrator
             // 
-            this.txtMenuButtonSwitchUser_Administrator.HeaderText = "SwitchUser";
-            this.txtMenuButtonSwitchUser_Administrator.Name = "txtMenuButtonSwitchUser_Administrator";
+            this.dgvMenuButtonLOGOUT_Administrator.BackColor = System.Drawing.Color.Black;
+            this.dgvMenuButtonLOGOUT_Administrator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvMenuButtonLOGOUT_Administrator.ForeColor = System.Drawing.Color.White;
+            this.dgvMenuButtonLOGOUT_Administrator.Location = new System.Drawing.Point(98, 484);
+            this.dgvMenuButtonLOGOUT_Administrator.Name = "dgvMenuButtonLOGOUT_Administrator";
+            this.dgvMenuButtonLOGOUT_Administrator.Size = new System.Drawing.Size(412, 46);
+            this.dgvMenuButtonLOGOUT_Administrator.TabIndex = 8;
+            this.dgvMenuButtonLOGOUT_Administrator.Text = "LOG OUT";
+            this.dgvMenuButtonLOGOUT_Administrator.UseVisualStyleBackColor = false;
+            // 
+            // lblMenuButton_Administrator
+            // 
+            this.lblMenuButton_Administrator.AutoSize = true;
+            this.lblMenuButton_Administrator.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMenuButton_Administrator.Location = new System.Drawing.Point(114, 145);
+            this.lblMenuButton_Administrator.Name = "lblMenuButton_Administrator";
+            this.lblMenuButton_Administrator.Size = new System.Drawing.Size(364, 25);
+            this.lblMenuButton_Administrator.TabIndex = 3;
+            this.lblMenuButton_Administrator.Text = "ADMIN USER SWITCH CONTROL";
             // 
             // MenuButton_Administrator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 601);
+            this.ClientSize = new System.Drawing.Size(634, 611);
             this.Controls.Add(this.grpMenuButton_Administrator);
             this.Name = "MenuButton_Administrator";
             this.Text = "MenuButton_Administrator";
             this.grpMenuButton_Administrator.ResumeLayout(false);
             this.grpMenuButton_Administrator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenuButton_Administrator)).EndInit();
             this.grbUserLogo_Administrator.ResumeLayout(false);
             this.grbUserLogo_Administrator.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picUserPhoto_Administrator)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvMenuButton_Administrator)).EndInit();
             this.ResumeLayout(false);
 
         }
